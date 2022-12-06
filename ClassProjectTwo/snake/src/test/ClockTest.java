ff745a7e3f23a20537d08730a2b1e4bc39588b53
aamer <66858114+goldGoat@users.noreply.github.com>
2022-12-05 20:51:09 -0800

    Adding files

---
+++ ClassProjectTwo/snake/src/test/ClockTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.psnbtech.Clock;
+
+class ClockTest {
+
+    @Test
+    public void testCycle() {
+        Clock clock = new Clock(9.0f);
+
+        assertFalse(clock.peekElapsedCycle());
+
+        try {
+			Thread.sleep(500);
+            clock.update();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+        assertTrue(clock.peekElapsedCycle());
+    }
+	
+	@Test
+	public void testIsP() {
+		Clock clock = new Clock(9.0f);
+		assertFalse(clock.isPaused());
+		clock.setPaused(true);
+		assertTrue(clock.isPaused());
+	}
+}
---
+++ ClassProjectTwo/snake/src/test/SnakeGameTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowEvent;
+
+import org.psnbtech.*;
+
+import org.junit.Test;
+
+public class SnakeGameTest {
+
+	@Test
+	public void testOutput() {
+
+		SnakeGame sg = new SnakeGame();
+		System.out.println("Testing the Maven project");
+		assertEquals(true,true);
+		
+		
+		
+}
+}
